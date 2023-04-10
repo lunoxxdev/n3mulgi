2e1d1c127d4d93cd298cf9ddb5cd1e554f44f742
lunoxxdev <128571018+lunoxxdev@users.noreply.github.com>
2023-04-11 03:15:50 +0700

    ngtd

--- Menu Final/cekv.sh
+++ Menu Final/cekv.sh
@@ -72,7 +72,7 @@ done
 rm -rf /tmp/other.txt
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
 echo -e "$COLOR1┌────────────────────── BY ───────────────────────┐${NC}"
-echo -e "$COLOR1│${NC}                 •  The Moon  •                  $COLOR1│$NC"
+echo -e "$COLOR1│${NC}                 •  Lunoxx Impostor  •                  $COLOR1│$NC"
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
 echo ""
 read -n 1 -s -r -p "   Press any key to back on menu"
