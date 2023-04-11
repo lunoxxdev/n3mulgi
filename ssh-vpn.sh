417638e9675324eb73ef414332a9f7caf68dd902
lunoxxdev <128571018+lunoxxdev@users.noreply.github.com>
2023-04-11 09:02:20 +0700

    ngtd

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -257,7 +257,7 @@ echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
 # banner /etc/issue.net
 sleep 1
 echo -e "[ ${green}INFO$NC ] Settings banner"
-wget -q -O /etc/issue.net "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/issue.net"
+wget -q -O /etc/issue.net "https://raw.githubusercontent.com/lunoxxdev/n3mulgi/main/issue.net"
 chmod +x /etc/issue.net
 echo "Banner /etc/issue.net" >> /etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
